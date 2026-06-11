179a038da0560bdfb953f1262d3ea25c405e7e1a
Arttu Åkerblom <arttuake@gmail.com>
2026-06-11 19:13:54 +0200

    starting profile skill filter

---
+++ app/components/LLSkillsFilter.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { SKILL_LABELS } from '~/utils/labels'
+
+const selectedSkills = ref<string[]>([])
+
+const skillsArray = Object.entries(SKILL_LABELS).map(([key, label]) => ({
+  key,
+  label
+}))
+</script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <UCheckbox
+      v-for="index in skillsArray"
+      :key="index.key"
+      :v-model="selectedSkills"
+      :label="index.label"
+      class="gap-1"
+    />
+  </div>
+</template>

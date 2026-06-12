e082a2c767ccad6d12fec3d2ca263e2150057f0c
Arttu Åkerblom <arttuake@gmail.com>
2026-06-12 16:52:23 +0200

    feat: add View more button to collapse and uncollapse skills filter

--- app/components/LLSkillsFilter.vue
+++ app/components/LLSkillsFilter.vue
@@ -3,18 +3,31 @@ import { SKILL_LABELS } from '~/utils/labels'
 
 // const selectedSkills = ref<string[]>([])
 const selectedSkills = defineModel<string[]>({ default: () => [] })
+const isExpanded = ref(false)
+
+const COLLAPSED_COUNT = 7
+
+const visibleSkills = computed(() => {
+  if (isExpanded.value)
+    return skillsArray
+  return skillsArray.slice(0, COLLAPSED_COUNT)
+})
 
 const skillsArray = Object.entries(SKILL_LABELS).map(([key, label]) => ({
   value: key,
   label: label
 }))
+
+if (!isExpanded.value)
+  skillsArray.slice(0, COLLAPSED_COUNT)
 </script>
 
 <template>
   <div class="flex flex-col gap-2">
     <UCheckboxGroup
       v-model="selectedSkills"
-      :items="skillsArray"
+      :items="visibleSkills"
     />
+    <UButton label="View more" @click="isExpanded = !isExpanded" />
   </div>
 </template>

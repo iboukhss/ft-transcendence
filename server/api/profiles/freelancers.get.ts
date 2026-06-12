1d58b3c900cfe86afb3d6ba49ecd371b6e0e4460
kevinnjaloung <kevinnjaloung@gmail.com>
2026-06-12 15:19:03 +0200

    added pagination feature

--- server/api/profiles/freelancers.get.ts
+++ server/api/profiles/freelancers.get.ts
@@ -1,5 +1,10 @@
 import { getFreelancerProfiles } from '#server/services/profile/get-freelancers.service.js'
 
-export default defineEventHandler(async () => {
-  return getFreelancerProfiles()
+export default defineEventHandler(async (event) => {
+  const { page } = getQuery(event)
+  console.log(page)
+  console.log(typeof page)
+  if (!page)
+    return ([])
+  return getFreelancerProfiles(parseInt(page as string))
 })
--- server/services/profile/get-freelancers.service.ts
+++ server/services/profile/get-freelancers.service.ts
@@ -2,11 +2,14 @@ import type { ProfileDTO } from '#shared/dto/profile.dto.js'
 
 import { profileSchema } from '#shared/dto/profile.dto.js'
 
-export async function getFreelancerProfiles(): Promise<ProfileDTO[]> {
+export async function getFreelancerProfiles(page: number): Promise<ProfileDTO[]> {
+  const data_limit = 4
+
   const records = await db
     .select()
     .from(tables.freelancerProfiles)
-
+    .offset(page * data_limit)
+    .limit(data_limit)
   return records.map((profileData) => {
     return profileSchema.parse({
       type: 'freelancer',

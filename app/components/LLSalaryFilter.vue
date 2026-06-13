3cf68b7a118d2525c7d38cdf0f1c38fcbd84b1e9
Arttu Åkerblom <arttuake@gmail.com>
2026-06-13 19:16:41 +0200

    start salary filter

---
+++ app/components/LLSalaryFilter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const minSalary = defineModel<number>({ default: 1 })
+const maxSalary = defineModel<number>({ default: 500 })
+
+const sliderValue = computed({
+  get() {
+    return [minSalary.value, maxSalary.value] as [number, number]
+  },
+  set(newValue) {
+    minSalary.value = newValue[0]
+    maxSalary.value = newValue[1]
+  }
+})
+</script>
+
+<template>
+  <LLFilterCollapsible label="Salary">
+    <USlider v-model="sliderValue" />
+  </LLFilterCollapsible>
+</template>

ffe44d290dd7d10811d6559d5e0d780d8851b7fb
vboxuser <kevinnjaloung@gmail.com>
2026-06-18 14:34:19 +0000

    added scroll up for profile page

--- app/pages/public/profiles/index.vue
+++ app/pages/public/profiles/index.vue
@@ -12,6 +12,7 @@ const { data: freelancers, refresh } = await useFetch<FreelancerDTO[]>(
 
 watch(page, async () => {
   await refresh()
+  window.scrollTo({ top: 0, behavior: 'smooth' })
 })
 const search = ref('')
 

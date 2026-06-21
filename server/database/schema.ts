ce19f3bbf33e11d393d6e176ccffee6ce0862a20
Ismail <151830328+iboukhss@users.noreply.github.com>
2026-06-21 14:37:09 +0200

    db: Fix and update database schema

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -15,23 +15,14 @@ import {
 } from '#shared/constants/enums'
 
 export const roleEnum = pgEnum('roleEnum', ROLE_KEYS)
-
 export const accountTypeEnum = pgEnum('accountTypeEnum', ACCOUNT_TYPE_KEYS)
-
 export const countryEnum = pgEnum('countryEnum', COUNTRY_KEYS)
-
 export const workPlaceEnum = pgEnum('workPlaceEnum', WORKPLACE_KEYS)
-
 export const languageEnum = pgEnum('languageEnum', LANGUAGE_KEYS)
-
 export const jobStatusEnum = pgEnum('jobStatusEnum', JOB_STATUS_KEYS)
-
 export const offerStatusEnum = pgEnum('offerStatusEnum', OFFER_STATUS_KEYS)
-
 export const bookingStatusEnum = pgEnum('bookingStatusEnum', BOOKING_STATUS_KEYS)
-
 export const categoryEnum = pgEnum('categoryEnum', JOB_CATEGORY_KEYS)
-
 export const skillsEnum = pgEnum('skillsEnum', SKILL_KEYS)
 
 export const users = pgTable('users', {
@@ -40,8 +31,8 @@ export const users = pgTable('users', {
   password: text('password').notNull(),
   accountType: accountTypeEnum('account_type').notNull(),
   role: roleEnum('role').notNull(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull()
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
 })
 
 export const freelancerProfiles = pgTable('freelancer_profiles', {
@@ -51,12 +42,12 @@ export const freelancerProfiles = pgTable('freelancer_profiles', {
   lastName: text('last_name').notNull(),
   country: countryEnum('country').notNull(),
   avatar: text('avatar'),
-  bio: text('bio'),
-  skills: text('skills').array().notNull(),
+  bio: text('bio').notNull().default(''),
+  skills: text('skills').array().notNull().default([]),
   languages: languageEnum('languages').array().notNull().default([]),
   hourlyRate: real('hourly_rate'),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull()
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
 })
 
 export const companyProfiles = pgTable('company_profiles', {
@@ -66,11 +57,11 @@ export const companyProfiles = pgTable('company_profiles', {
   contactLastName: text('contact_last_name').notNull(),
   companyName: text('company_name').notNull(),
   country: countryEnum('country').notNull(),
-  website: text('website'),
+  website: text('website').notNull().default(''),
   logo: text('logo'),
-  description: text('description'),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull()
+  description: text('description').notNull().default(''),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
 })
 
 export const jobs = pgTable('jobs', {
@@ -82,64 +73,55 @@ export const jobs = pgTable('jobs', {
 
   // We lose a little typesafety here for a more confortable query experience.
   // This row used to be a PgEnum array.
-  skills: text('skills').array().notNull(),
+  skills: text('skills').array().notNull().default([]),
 
   hourlyRate: real('hourly_rate').notNull(),
-  duration: integer('duration').default(1).notNull(),
+  duration: integer('duration').notNull(),
   workplace: workPlaceEnum('workplace').notNull(),
   location: countryEnum('location').notNull(),
-  status: jobStatusEnum('status').default('active').notNull(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull()
+  status: jobStatusEnum('status').notNull().default('active'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
 })
 
 export const bookings = pgTable('bookings', {
   id: serial('id').primaryKey(),
-  offerId: integer('offer_id').notNull().references(() => offers.id),
+  offerId: integer('offer_id').notNull().references(() => offers.id).unique(),
   jobId: integer('job_id').notNull().references(() => jobs.id, { onDelete: 'cascade' }),
   buyerId: integer('buyer_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
   sellerId: integer('seller_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
   price: real('price').notNull(),
   hourlyRate: real('hourly_rate').notNull(),
   duration: integer('duration').notNull(),
-  workplace: workPlaceEnum('workplace'),
-  status: bookingStatusEnum('status').default('upcoming').notNull(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull()
-}, table => ({
-  oncePerOffer: unique().on(table.offerId)
-}))
+  status: bookingStatusEnum('status').notNull().default('upcoming'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
 
 export const offers = pgTable('offers', {
   id: serial('id').primaryKey(),
   jobId: integer('job_id').notNull().references(() => jobs.id, { onDelete: 'cascade' }),
   buyerId: integer('buyer_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
   sellerId: integer('seller_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
-  status: offerStatusEnum('status').default('pending').notNull(),
+  status: offerStatusEnum('status').notNull().default('pending'),
   motivationLetter: text('motivation_letter').notNull(),
   proposedHourlyRate: real('proposed_hourly_rate').notNull(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull()
-}, (table) => {
-  return {
-    oncePerOffer: unique().on(table.jobId, table.sellerId)
-  }
-})
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
+}, table => [
+  unique('offers_job_id_seller_id_unique').on(table.jobId, table.sellerId)
+])
 
 export const apiKeys = pgTable('api_keys', {
   id: serial('id').primaryKey(),
-  userId: integer('user_id')
-    .notNull()
-    .references(() => users.id, { onDelete: 'cascade' }),
+  userId: integer('user_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
   key: text('key').notNull().unique(),
   name: text('name').notNull().default('Default Key'),
-
   isActive: boolean('is_active').notNull().default(true),
   lastUsedAt: timestamp('last_used_at'),
   expiresAt: timestamp('expires_at'),
-
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull()
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow()
 })
 
 export const offersRelations = relations(offers, ({ one }) => ({
